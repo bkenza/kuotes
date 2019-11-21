8172d50cad443d88b65c3fee79b9b1de3bb25546
Kenza <kenza.boulisfane@gmail.com>
2019-11-20 19:54:49 -0500

    add toggle contrast funtionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
+// Get quote from endpoint
 function getQuote() {
   fetch(endpoint)
     .then(function (response) {
@@ -13,6 +14,7 @@ function getQuote() {
     });
 }
 
+// display quote
 function displayQuote(quote) {
   const quoteText = document.querySelector('.quote-text');
   quoteText.textContent = quote;
@@ -21,7 +23,17 @@ function displayQuote(quote) {
   tweetButton.setAttribute('href', `https://twitter.com/share?text=${quote} - Donald Trump`);
 }
 
+// toggle contrast of the website
+function changeTheme() {
+  document.body.classList.toggle('light-theme');
+}
+
 const newQuoteButton = document.querySelector('.new-quote');
 newQuoteButton.addEventListener('click', getQuote);
+
+const changeThemeButton = document.getElementById('change-theme-btn');
+changeThemeButton.addEventListener('click', changeTheme)
+
+
 
 getQuote();
